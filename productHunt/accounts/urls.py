4411e3f54b7ed2fa021c9009a27fa02a0d60e357
mohammadjavadArdestani <mjavad.ardestani00@gmail.com>
2020-02-15 17:03:13 +0330

    login logout signup added

--- productHunt/accounts/urls.py
+++ productHunt/accounts/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('login',views.login,name='login'),
-    path('signup',views.signup,name='signup'),
-    path('logout',views.signup,name='logout'),
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
+    path('logout/',views.logout,name='logout'),
     
 ]
--- productHunt/accounts/views.py
+++ productHunt/accounts/views.py
@@ -1,11 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth 
 
 # Create your views here.
 def signup(request):
+ if request.method=='POST':
+     if request.POST['password1']==request.POST['password2']:
+         print("fuck fuck*****************************")
+         try:
+             user=User.objects.get(username=request.POST['username'])
+             return render(request,'accounts/signup.html',{'error':'username has been taken'})
+         except User.DoesNotExist:
+             user= User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+             auth.login(request,user)
+             #return redirect('home')
+             #print("fuck")
+             return render(request,'products/home.html')      
+     else:
+         return render(request,'accounts/signup.html',{'error':'you didint confirm password correctly!'})
+
+        
  return render(request,'accounts/signup.html')   
 
 def login(request):
- return render(request,'accounts/login.html')   
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'] ,password=request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return render(request,'products/home.html')      
+        else:
+            return render(request,'accounts/login.html',{'error':'username or password maybe false'})   
+    else:        
+        return render(request,'accounts/login.html')   
 
 def logout(request):
- return render(request,'accounts/signup.html')   
+    if request.method == 'POST':
+        logout(request)
+        return render(request,'products/login.html')  
+    else:
+        return render(request,'accounts/login.html')   
